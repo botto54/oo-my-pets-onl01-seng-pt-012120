b4e54cf1d6dc305d51947a0b495dcff0d718b14b
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-07 17:55:31 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,13 @@
 class Owner
-  # code goes here
+  @@all = []
+  attr_accessor :name, :pets
+  attr_reader :species
+  
+  def initialize
+    @species = species
+    @@all << self
+    @pets = {:dogs => [], :cats => []}
+  end
+  
+  def say_species
 end
