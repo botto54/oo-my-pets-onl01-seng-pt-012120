c07b26d07ec319fa4495895270b6ee9cd11b4890
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-07 20:04:21 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -55,8 +55,17 @@ class Owner
   end
   
   def sell_pets
-    self.cats.each {|cat| cat.mood = "nervous", cat.owner = nil}
-    self.cats.delete(cat)
+    self.cats.each do |cat|
+      self.cats.delete(cat)
+      cat.owner = nil
+      cat.mood = "nervous"
+    end
+    
+    self.dogs.each do |dog|
+      self.dogs.delete(dog)
+      dog.owner = nil
+      dog.mood = "nervous"
+    end
   end
   
   def list_pets
