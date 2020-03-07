e71d26d4f1f706f961f8972ce0669dcdd33e89e0
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-07 18:34:26 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,11 +1,15 @@
 class Cat
   attr_accessor :mood
-  attr_reader :name, :owner
+  attr_reader :name
   
   def initialize(name, owner)
     @name = name
     @owner = owner.name
     @mood = "nervous"
+  end
+  
+  def self.owner
+    
   end
   
 end
